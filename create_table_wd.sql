712b8ac665d6abf86b2972d8b16eff98e24b121b
Tor Hveem <tor@hveem.no>
2013-01-27 13:53:36 +0100

    poller and sql schema

---
+++ create_table_wd.sql
@@ -0,0 +1,14 @@
+CREATE TABLE "wd" (
+    "id" serial NOT NULL,
+    "timestamp" timestamp UNIQUE,
+    "avg_speed" real NOT NULL,
+    "gusts" real NOT NULL,
+    "winddir" smallint NOT NULL,
+    "temp" real NOT NULL,
+    "hum" smallint NOT NULL,
+    "barometer" real NOT NULL,
+    "daily_rain" real NOT NULL,
+    "dew_temp" real NOT NULL,
+    "cloud_height" real NOT NULL
+);
+
