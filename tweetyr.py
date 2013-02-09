d6b9be8145316f6f90e47bb3a55c861f993a375a
Tor Hveem <tor@hveem.no>
2013-02-09 15:53:59 +0100

    Add geo info to status update

--- tweetyr.py
+++ tweetyr.py
@@ -8,14 +8,14 @@ import json
 from urllib2 import urlopen
 import os
 
-root =os.path.dirname(os.path.abspath(__file__))
-
+root = os.path.dirname(os.path.abspath(__file__))
 conf = json.loads(file(root+'/twitterconfig.json').read())
 
 auth = tweepy.OAuthHandler(conf['consumerkey'], conf['consumersecret'])
 auth.set_access_token(conf['accesstoken'], conf['accesssecret'])
+
 api = tweepy.API(auth)
 
 w = json.loads(urlopen(conf['apiurl']).read())[0]
 
-api.update_status('%(outtemp).1f °C, %(windspeed).1f m/s vind, %(rain).1f mm nedbør' %w);
+api.update_status('%(outtemp).1f °C, %(windspeed).1f m/s vind, %(rain).1f mm nedbør' %w,lat=conf['lat'],long=conf['long'])
