178c173d519637504edb9f9f1dd2e07b73e3022a
Tor Hveem <tor@hveem.no>
2013-11-04 12:33:36 +0100

    add wind scale to twitter

--- tweetyr.py
+++ tweetyr.py
@@ -29,4 +29,40 @@ if len(sys.argv) > 1:
     filename = open(conf['snapfile'])
     api.update_status_with_media(filename, status=sys.argv[1] + ' %(outtemp).1f °C, %(windspeed).1f m/s vind, %(dayrain).1f mm nedbør' %w,lat=conf['lat'],long=conf['long'])
 else:
-    api.update_status('%(outtemp).1f °C, %(windspeed).1f m/s vind, %(dayrain).1f mm nedbør' %w,lat=conf['lat'],long=conf['long'])
+    out = []
+    out.append('%(outtemp).1f °C' %w)
+
+    ws = w['windspeed']
+
+    if ws > 32:
+        out.append('orkan! (%(windspeed).1f m/s)' %w)
+    elif ws > 28.5:
+        out.append('sterk storm (%(windspeed).1f m/s)' %w)
+    elif ws > 24.5:
+        out.append('full storm (%(windspeed).1f m/s)' %w)
+    elif ws > 20.8:
+        out.append('liten storm (%(windspeed).1f m/s)' %w)
+    elif ws > 17.2:
+        out.append('sterk kuling (%(windspeed).1f m/s)' %w)
+    elif ws > 13.9:
+        out.append('stiv kuling (%(windspeed).1f m/s)' %w)
+    elif ws > 10.8:
+        out.append('liten kuling (%(windspeed).1f m/s)' %w)
+    elif ws > 8:
+        out.append('frisk bris (%(windspeed).1f m/s)' %w)
+    elif ws > 5.5:
+        out.append('laber bris (%(windspeed).1f m/s)' %w)
+    elif ws > 3.4:
+        out.append('lett bris (%(windspeed).1f m/s)' %w)
+    elif ws > 1.5:
+        out.append('svak vind (%(windspeed).1f m/s)' %w)
+    elif ws > 0.3:
+        out.append('flau vind (%(windspeed).1f m/s)' %w)
+    else:
+        out.append('vindstille')
+    # TODO add text for gusts
+
+    if w['dayrain'] > 0:
+        out.append('%(dayrain).1f mm nedbør' %w)
+
+    api.update_status(", ".join(out), lat=conf['lat'], long=conf['long'])
